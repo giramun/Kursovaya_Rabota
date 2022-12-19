d79e18bbfd9d3099edfbb24724f1980c90d646f8
giramun <giramuncsgo@gmail.com>
2022-12-19 20:56:49 +0300

    Added header for the next pages!

---
+++ header.php
@@ -0,0 +1,6 @@
+<div id="header">
+    <div class="header-element"><a href="/">HOME</a></div>
+    <div class="header-element"><a href="vehicles.php">VEHICLES</a></div>
+    <div class="header-element"><a href="request.php">CAR REQUEST</a></div>
+    <div class="header-element"><a href="contactus.php">CONTACT US</a></div>
+</div>
