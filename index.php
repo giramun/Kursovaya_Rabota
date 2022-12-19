5a6dd7d3fced7c582f699202b0c8b5217a0df508
giramun <giramuncsgo@gmail.com>
2022-12-19 20:54:15 +0300

    Added header for the next pages!

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 <html lang="ru">
 <head>
     <title>Dealer CRM</title>
+    <link type="text/css" rel="stylesheet" href="style.css" />
 </head>
 <body>
+<?php require 'header.php' ?>
     <h1>Dealer CRM</h1>
 </body>
 </html>
