700a111d98613368b6534d38c2cdc55a43662db4
KimAP_IzmailovEA <kimap@student.bmstu.ru>
2022-12-26 00:15:35 +0300

    Completed request page!

--- request.php
+++ request.php
@@ -7,7 +7,18 @@
 <body>
 <?php require 'header.php' ?>
 <div id="mainpage">
- <h1>asdasda</h1>
+ <h1 style="text-align: center">Car Request</h1>
+    <form action="" method="post">
+        <label>Name</label>
+        <input type="text" name="name" required/>
+        <label>E-mail</label>
+        <input type="email" name="e-mail" required/>
+        <label>Phone number</label>
+        <input type="number" name="number" required/>
+        <label>Tell us about requested vehicle</label>
+        <textarea></textarea>
+        <input type="submit" name="SUBMIT"/>
+    </form>
 </div>
 <?php require 'footer.php' ?>
 </body>
