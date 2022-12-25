287ef43492c7ece0d2d6c4039743fdfc3df3398c
KimAP_IzmailovEA <kimap@student.bmstu.ru>
2022-12-26 00:36:19 +0300

    Fixed request page! Added classes and attributes to some tags.

--- request.php
+++ request.php
@@ -8,16 +8,24 @@
 <?php require 'header.php' ?>
 <div id="mainpage">
  <h1 style="text-align: center">Car Request</h1>
-    <form action="" method="post">
-        <label>Name</label>
-        <input type="text" name="name" required/>
-        <label>E-mail</label>
-        <input type="email" name="e-mail" required/>
-        <label>Phone number</label>
-        <input type="number" name="number" required/>
-        <label>Tell us about requested vehicle</label>
-        <textarea></textarea>
-        <input type="submit" name="SUBMIT"/>
+    <form action="" method="post" id="request-form">
+        <div class="form-label">
+            <label>Name</label>
+            <input type="text" name="name" required/>
+        </div>
+            <div class="form-label"l>E-mail</label>
+            <input type="email" name="e-mail" required/>
+        </div>
+        <div class="form-label">
+            <label>Phone number</label>
+            <input type="number" name="number" required/>
+        </div>
+        <div class="center">
+            <textarea required placeholder="Tell us about requested car"></textarea>
+        </div>
+        <div class="center button">
+            <button type="submit">SUBMIT</button>
+        </div>
     </form>
 </div>
 <?php require 'footer.php' ?>
